c9e4437d24f93a450f2c6fc3e3489ad3d37c7f26
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2025-12-04 08:54:48 +0000

    Adjust loop indices

--- 03/a.py
+++ 03/a.py
@@ -7,7 +7,7 @@ total = 0
 
 for bank in banks:
     largest = 0
-    for i in range(len(bank) - 1):
+    for i in range(len(bank)):
         for j in range(i + 1, len(bank)):
             joltage = int(bank[i] + bank[j])
             if joltage > largest:
